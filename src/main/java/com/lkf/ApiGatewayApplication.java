13fbbe41208435ac6beea7cb41bf72c5a0de9ec0
likaifeng <844701087@qq.com>
2017-07-31 15:43:28 +0800

    api 网关

---
+++ src/main/java/com/lkf/ApiGatewayApplication.java
@@ -0,0 +1,21 @@
+package com.lkf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ *spring cloud zuul(网关)
+ *@author: lkf
+ *@Date: 2017/7/31 15:20
+ */
+@EnableZuulProxy
+@EnableEurekaClient
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+}
